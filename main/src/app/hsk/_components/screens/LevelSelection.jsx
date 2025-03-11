6b526c0252f28a6f39e0c6817dfb0909cf71a4c5
TarinPairor <111269625+TarinPairor@users.noreply.github.com>
2025-03-12 01:14:15 +0800

    FRename hsk roleplay to hsk mode

--- main/src/app/hsk/_components/screens/LevelSelection.jsx
+++ main/src/app/hsk/_components/screens/LevelSelection.jsx
@@ -8,21 +8,22 @@ export default function LevelSelection({ hskLevels, setSelectedLevel }) {
         <Spline scene="https://prod.spline.design/ZCLA7cXZ95aBo0pU/scene.splinecode" />
       </div>
       <h1 className="main-title">
-        HSK Roleplay
+        HSK Mode
         <br />
         <span>With Xiao Qiu</span>
       </h1>
       <h2 className="subtitle">Select Your HSK Level:</h2>
       <div className="circle-row">
-        {hskLevels && hskLevels.map((hsk) => (
-          <div
-            key={hsk.level}
-            className="circle"
-            onClick={() => setSelectedLevel(hsk.level)}
-          >
-            <p>HSK {hsk.level}</p>
-          </div>
-        ))}
+        {hskLevels &&
+          hskLevels.map((hsk) => (
+            <div
+              key={hsk.level}
+              className="circle"
+              onClick={() => setSelectedLevel(hsk.level)}
+            >
+              <p>HSK {hsk.level}</p>
+            </div>
+          ))}
       </div>
     </div>
   );
